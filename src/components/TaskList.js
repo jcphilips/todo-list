40530169250f6e04f1034c6e16bb88fe35218584
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2023-11-26 19:25:26 +0530

    add localStorage and task deletion confirmation

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,24 @@
-export default function TaskList({ handleDelete, taskList, parent }) {
+import { ConfirmDialog } from 'primereact/confirmdialog';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
+export default function TaskList({ handleDelete, taskList, parent, setVisible, visible }) {
     return (
         <ul ref={parent}>
             {taskList.map(({task, id}) => (
                     <li key={id}>
                         <p>{task}</p>
-                        <input type="button" value="&#9747;" onClick={() => handleDelete(id)} className="delete-task" />
+                        <ConfirmDialog visible={visible} onHide={() => setVisible(false)} message="Are you sure you want to delete the task?"
+    header="Confirmation" icon="pi pi-exclamation-triangle" accept={() => handleDelete(id)} reject={() => setVisible(false)} />
+                        <input 
+                            type="button" 
+                            value="&#9747;" 
+                            onClick={() => {
+                                setVisible(true)
+                            }} 
+                            className="delete-task" 
+                        />
                     </li>
             ))}
         </ul>
--- src/containers/TaskContainer.js
+++ src/containers/TaskContainer.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NewTask from "../components/NewTask";
 import TaskList from "../components/TaskList";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+
 
 export default function TaskContainer() {
     const [newTask, setNewTask] = useState({
         task: '',
         id: null
     });
-    const [taskList, setTaskList] = useState([]);
+    const [taskList, setTaskList] = useState(() => {
+        const temp = localStorage.getItem('taskList');
+        const savedTodos = JSON.parse(temp);
+        return (savedTodos || []);
+    });
+    const [visible, setVisible] = useState(false);
+
     const [parent, enableAnimations] = useAutoAnimate();
+
+    useEffect(() => {
+        const temp = JSON.stringify(taskList);
+        localStorage.setItem('taskList', temp);
+    }, [taskList]);
+
 
     const handleChange = ({ target }) => {
         const { name, value } = target;
@@ -36,7 +49,7 @@ export default function TaskContainer() {
     return (
         <div>
             <NewTask handleChange={handleChange} handleSubmit={handleSubmit} newTask={newTask} />
-            <TaskList handleDelete={handleDelete} taskList={taskList} parent={parent} />
+            <TaskList handleDelete={handleDelete} taskList={taskList} parent={parent} setVisible={setVisible} visible={visible} />
         </div>
     )
 
