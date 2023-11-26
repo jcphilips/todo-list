fcf6d9fa9baa1613515af7808893e5431c30e427
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2023-11-26 12:37:05 +0530

    improve ux

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,18 +1,19 @@
 export default function NewTask({ handleChange, handleSubmit, newTask }) {
     return (
         <div className="create-new-task">
+            <h1>Create a task: </h1>
             <form onSubmit={handleSubmit}>
-                <h1>Create a task: </h1>
-                <input 
-                    id='task-details' 
+                <input
                     type="text" 
+                    id='task-details' 
                     name="task" 
                     onChange={handleChange} 
                     value={newTask.task || ''} 
                     autoComplete="off" 
-                    placeholder="Enter your task" 
-                    
+                    placeholder="Enter your task..." 
+                    style={{ height: Math.max() }}
                 />
+                <input type="submit" value='Create Todo' id="submit-todo"/>
             </form>
         </div>
     )
